b458b457db7f0eb1d89bbd287de1cbe1deb83b0c
3 Moon <3moon@3s-MacBook-Pro-2.local>
2022-12-31 03:24:37 -0500

    update server url

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv)
 
   //fetch data from server
-  const response = await fetch('http://localhost:5010', {
+  const response = await fetch('https://codex-vcvj.onrender.com', {
     method: 'POST',
     headers: {
        'Content-Type': 'application/json',
